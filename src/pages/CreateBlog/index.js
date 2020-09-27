c799817a2ec719d9959ae713113f19ddd62c1f09
dedesaepulloh <dedesaepulloh77@gmail.com>
2020-09-27 20:57:49 +0700

    nested routes MainApp page

---
+++ src/pages/CreateBlog/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateBlog = () => {
+    return (
+        <div>
+            <h2>Content Create Blog Page</h2>
+        </div>
+    )
+}
+
+export default CreateBlog
---
+++ src/pages/DetailBlog/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DetailBlog = () => {
+    return (
+        <div>
+            <h2>Content Detail Blog Page</h2>
+        </div>
+    )
+}
+
+export default DetailBlog
