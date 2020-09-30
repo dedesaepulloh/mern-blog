886c5c5fdd38e22fcdb3d277b7b12f7e9cf273fa
dedesaepulloh <dedesaepulloh77@gmail.com>
2020-09-30 09:14:50 +0700

    styling detail blog post

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { RegisterBg } from '../../assets';
+import { Gap } from '../../components';
+import './detailBlog.scss';
 
 const DetailBlog = () => {
     return (
-        <div>
-            <h2>Content Detail Blog Page</h2>
+        <div className="detail-blog-wrapper">
+            <Gap height={30} />
+            <img className="img-cover" src={RegisterBg} alt="thumb" />
+            <p className="blog-title">Title Blog</p>
+            <p className="blog-author">Author - Date Post</p>
+            <p className="blog-body">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia, soluta, laborum reprehenderit, animi necessitatibus impedit laudantium earum enim voluptatibus minima unde quasi tempore quam maxime ut expedita non quo.</p>
         </div>
     )
 }
